0d74b17ae829bb0e3191047f2ccb09beceb6aa69
Suhas Parameshwara <suhas.parameshwara@kovai.co>
2021-10-24 19:31:27 +0530

    removed details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: true,
-  azureConnection: "Endpoint=https://testdemo.service.signalr.net;AccessKey=++xf5L0+WITGURG4QNKSYS4d+QEidpMACoM9ekN2Uns=;Version=1.0;"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-  azureConnection: "Endpoint=https://testdemo.service.signalr.net;AccessKey=++xf5L0+WITGURG4QNKSYS4d+QEidpMACoM9ekN2Uns=;Version=1.0;"
 };
 
 /*
